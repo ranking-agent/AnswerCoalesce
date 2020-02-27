2a2ef3938278734811747f8fc84c8f547a593edf
cbizon <bizon@renci.org>
2020-02-27 15:47:08 -0500

    Fixed Test.

--- tests/test_node_coalescer.py
+++ tests/test_node_coalescer.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import os
 import src.single_node_coalescer as snc
+from collections import defaultdict
 
 def test_bindings():
     """Load up the answer in robokop_one_hop.json.
@@ -62,7 +63,7 @@ def test_hash_one_hop_with_different_predicates():
     assert len(s) == len(preds)
     assert len(s) > 1 #there better be more than one
 
-def xtest_hash_topology():
+def test_hash_topology():
     """This question has a more complicated topology.  There are two gene nodes that never
     change.  The other gene spot varies, and has related process variation.  Most variations
     include only one gene, but there are a couple that include 2"""
@@ -73,13 +74,14 @@ def xtest_hash_topology():
     qg = answerset['question_graph']
     kg = answerset['knowledge_graph']
     answers = answerset['answers']
-    s = {}
+    s = defaultdict(list)
     for i,a in enumerate(answers):
         bindings = snc.make_bindings(qg,a)
         s[snc.make_answer_hash(bindings,kg,qg,'n7')].append(i)
     nb = 0
-    for b,l in s:
+    #If n7 is allowed to vary, then we're looking for gene groupings, of which there are 4.
+    for b,l in s.items():
         if len(l) > 1:
             nb += 1
-    assert nb==2
+    assert nb==4
 
