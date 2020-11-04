e8692c2b9abbe56648cb71c43db2df1d2e665e48
Chris Bizon <bizon@rciadmins-MacBook-Pro.local>
2020-11-04 16:06:12 -0500

    new test

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -67,3 +67,25 @@ def test_unique_coalesce():
 
     assert('results' in ret)
     assert( len(ret['results']) <= 4 )
+
+def test_schizo_coalesce():
+    dir_path: str = os.path.dirname(os.path.realpath(__file__))
+    testfilename = os.path.join(dir_path, 'schizo.json')
+
+    with open(testfilename, 'r') as tf:
+        answerset = json.load(tf)
+
+    # setup some parameters
+    param = {'method': 'graph'}
+
+    # make a good request
+    request, response = app.test_client.post('/coalesce', params= param, data=json.dumps(answerset))
+
+    # was the request successful
+    assert(response.status == 200)
+
+    # convert the response to a json object
+    ret = json.loads(response.body)
+
+    #assert('results' in ret)
+    #assert( len(ret['results']) <= 4 )
