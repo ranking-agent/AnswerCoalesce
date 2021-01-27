b4afc916d97ea0253367f8071e5f492b1e130d6f
Phil Owen <phillipsowen@outlook.com>
2021-01-27 13:47:06 -0500

    restore the correct output

--- src/server.py
+++ src/server.py
@@ -53,10 +53,10 @@ async def coalesce_handler(response: Response, method: MethodName) -> Response:
     coalesced = coalesce(message, method=method)
 
     # Normalize the output
-    # normed = normalize( {'message': coalesced } )
+    normed = normalize( {'message': coalesced } )
 
     # return the result to the caller
-    return Response(**{'message': coalesced })
+    return Response(**normed)
 
 
 def log_exception(method):
