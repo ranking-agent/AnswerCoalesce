e098b6199f249994c3506506dede365c7fc3c3d9
Phil Owen <phillipsowen@outlook.com>
2021-04-16 10:26:56 -0400

    now showing Chris as owner of AC

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 setup(
     name='Answer Coalesce',
     version='1.0.0',
-    author='Phil Owen',
-    author_email='powen@renci.org',
+    author='Chris Bizon',
+    author_email='bizon@renci.org',
     url='https://github.com/TranslatorIIPrototypes/AnswerCoalesce',
-    description='Answer Coalesce - Offers coalesced answers based on the answer passed in.',
+    description='Answer Coalesce - Offers coalesced answers based on an answer passed in.',
     packages=['ac'],
     include_package_data=True,
     zip_safe=False,
