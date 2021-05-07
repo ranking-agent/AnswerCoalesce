ec87425b00120db0620a52b1706bd57d5c346892
Phil Owen <phillipsowen@outlook.com>
2021-05-07 14:49:06 -0400

    altering to change root dir to the trapi version

--- main.sh
+++ main.sh
@@ -2,4 +2,6 @@
 
 # export $(egrep -v '^#' .env | xargs)
 
-gunicorn --bind 0.0.0.0:6380 -w 1 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP
+# gunicorn --bind 0.0.0.0:6380 -w 1 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP --root-path /1.1
+
+uvicorn --host 0.0.0.0 --port 6380 --workers 1 src.server:APP --root-path /1.1
