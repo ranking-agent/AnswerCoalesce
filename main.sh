87e71d6d83e148d808a0abd85ab467c222efdb5a
Phil Owen <phillipsowen@outlook.com>
2021-09-13 16:18:13 -0400

    fixing path to 1.2 endpoint

--- main.sh
+++ main.sh
@@ -2,6 +2,6 @@
 
 # export $(egrep -v '^#' .env | xargs)
 
-# gunicorn --bind 0.0.0.0:6380 -w 1 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP --root-path /1.1
+# gunicorn --bind 0.0.0.0:6380 -w 1 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP --root-path /1.2
 
-uvicorn --host 0.0.0.0 --port 6380 --workers 1 src.server:APP --root-path /1.1
+uvicorn --host 0.0.0.0 --port 6380 --workers 1 src.server:APP --root-path /1.2
