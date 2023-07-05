cdffe83804e47c46ae5ce80ea63a9cc4fcb1f177
Olawumi <wumirose@gmail.com>
2023-07-05 11:42:12 -0400

    I/O pydantic compliance

--- tests/upgrade_trapi.py
+++ tests/upgrade_trapi.py
@@ -6,31 +6,7 @@ from pydantic import BaseModel
 from enum import Enum
 
 
-class source_items(str, Enum):
-    resource_id: str
-    resource_role: str
-    upstream_resource_ids: str
-
-
-class Source(BaseModel):
-    sources: List[source_items]
-
-def upgrade_BiolinkEntity(biolink_entity):
-    """Upgrade BiolinkEntity from 0.9.2 to 1.0.0."""
-    if biolink_entity.startswith("biolink:"):
-        return biolink_entity
-    return "biolink:" + pascal_case(biolink_entity)
-
-
-def upgrade_BiolinkRelation(biolink_relation):
-    """Upgrade BiolinkRelation (0.9.2) to BiolinkPredicate (1.0.0)."""
-    if biolink_relation is None:
-        return None
-    if biolink_relation.startswith("biolink:"):
-        return biolink_relation
-    return "biolink:" + snake_case(biolink_relation)
-
-
+# Currently in use
 def upgrade_Node(node):
     """Upgrade Node from 0.9.2 to 1.0.0."""
     node = {**node}
@@ -59,6 +35,7 @@ def upgrade_Node(node):
     return new
 
 
+# Currently in use
 def upgrade_Edge(edge):
     """Upgrade Edge from 0.9.2 to 1.4.0."""
     possible_roles = ('aggregator_knowledge_source', 'primary_knowledge_source', 'supporting_data_source')
@@ -122,83 +99,8 @@ def upgrade_KnowledgeGraph(kgraph):
     }
 
 
-def upgrade_QNode(qnode):
-    """Upgrade QNode from 0.9.2 to 1.0.0."""
-    qnode = {**qnode}
-    qnode.pop("id")
-    new = dict()
-    if "type" in qnode:
-        new["category"] = upgrade_BiolinkEntity(qnode.pop("type"))
-    if "curie" in qnode:
-        new["id"] = qnode.pop("curie")
-    # add remaining properties verbatim
-    new = {
-        **new,
-        **qnode,
-    }
-    return new
 
-
-def upgrade_QEdge(qedge):
-    """Upgrade QEdge from 0.9.2 to 1.0.0."""
-    qedge = {**qedge}
-    qedge.pop("id")
-    new = {
-        "subject": qedge.pop("source_id"),
-        "object": qedge.pop("target_id"),
-    }
-    if "type" in qedge:
-        new["predicate"] = upgrade_BiolinkRelation(qedge.pop("type"))
-    if "relation" in qedge:
-        new["relation"] = qedge.pop("relation")
-    # add remaining properties verbatim
-    new = {
-        **new,
-        **qedge,
-    }
-    return new
-
-
-def upgrade_QueryGraph(qgraph):
-    """Upgrade QueryGraph from 0.9.2 to 1.0.0."""
-    return {
-        "nodes": {
-            qnode["id"]: upgrade_QNode(qnode)
-            for qnode in qgraph["nodes"]
-        },
-        "edges": {
-            qedge["id"]: upgrade_QEdge(qedge)
-            for qedge in qgraph["edges"]
-        },
-    }
-
-
-def upgrade_NodeBinding(node_binding):
-    """Upgrade NodeBinding from 0.9.2 to 1.0.0."""
-    for kg_id in ensure_list(node_binding["kg_id"]):
-        new = {
-            "id": kg_id,
-        }
-        for key, value in node_binding.items():
-            if key in ("qg_id", "kg_id"):
-                continue
-            new[key] = value
-        yield new
-
-
-def upgrade_EdgeBinding(edge_binding):
-    """Upgrade EdgeBinding from 0.9.2 to 1.0.0."""
-    for kg_id in ensure_list(edge_binding["kg_id"]):
-        new = {
-            "id": kg_id,
-        }
-        for key, value in edge_binding.items():
-            if key in ("qg_id", "kg_id"):
-                continue
-            new[key] = value
-        yield new
-
-
+# Currently in use
 def upgrade_results(results):
     if isinstance(results, list):
         upgraded_trapi1_4 = []
@@ -227,6 +129,115 @@ def upgrade_results(results):
     return upgraded_trapi1_4
 
 
+# Currently in use
+def upgrade_Message(message):
+    """Upgrade Message from 0.9.2 to 1.0.0."""
+    new = dict()
+    if "query_graph" in message:
+        new["query_graph"] = message["query_graph"]
+    if "knowledge_graph" in message:
+        new["knowledge_graph"] = upgrade_KnowledgeGraph(message["knowledge_graph"])
+    if "results" in message:
+        new["results"] = upgrade_results(message["results"])
+    return new
+
+
+# Not in use
+def upgrade_BiolinkEntity(biolink_entity):
+    """Upgrade BiolinkEntity from 0.9.2 to 1.0.0."""
+    if biolink_entity.startswith("biolink:"):
+        return biolink_entity
+    return "biolink:" + pascal_case(biolink_entity)
+
+# Not in use
+def upgrade_BiolinkRelation(biolink_relation):
+    """Upgrade BiolinkRelation (0.9.2) to BiolinkPredicate (1.0.0)."""
+    if biolink_relation is None:
+        return None
+    if biolink_relation.startswith("biolink:"):
+        return biolink_relation
+    return "biolink:" + snake_case(biolink_relation)
+
+# Not in use
+def upgrade_QNode(qnode):
+    """Upgrade QNode from 0.9.2 to 1.0.0."""
+    qnode = {**qnode}
+    qnode.pop("id")
+    new = dict()
+    if "type" in qnode:
+        new["category"] = upgrade_BiolinkEntity(qnode.pop("type"))
+    if "curie" in qnode:
+        new["id"] = qnode.pop("curie")
+    # add remaining properties verbatim
+    new = {
+        **new,
+        **qnode,
+    }
+    return new
+
+
+# Not in use
+def upgrade_QEdge(qedge):
+    """Upgrade QEdge from 0.9.2 to 1.0.0."""
+    qedge = {**qedge}
+    qedge.pop("id")
+    new = {
+        "subject": qedge.pop("source_id"),
+        "object": qedge.pop("target_id"),
+    }
+    if "type" in qedge:
+        new["predicate"] = upgrade_BiolinkRelation(qedge.pop("type"))
+    if "relation" in qedge:
+        new["relation"] = qedge.pop("relation")
+    # add remaining properties verbatim
+    new = {
+        **new,
+        **qedge,
+    }
+    return new
+
+# Not in use
+def upgrade_QueryGraph(qgraph):
+    """Upgrade QueryGraph from 0.9.2 to 1.0.0."""
+    return {
+        "nodes": {
+            qnode["id"]: upgrade_QNode(qnode)
+            for qnode in qgraph["nodes"]
+        },
+        "edges": {
+            qedge["id"]: upgrade_QEdge(qedge)
+            for qedge in qgraph["edges"]
+        },
+    }
+
+# Not in use
+def upgrade_NodeBinding(node_binding):
+    """Upgrade NodeBinding from 0.9.2 to 1.0.0."""
+    for kg_id in ensure_list(node_binding["kg_id"]):
+        new = {
+            "id": kg_id,
+        }
+        for key, value in node_binding.items():
+            if key in ("qg_id", "kg_id"):
+                continue
+            new[key] = value
+        yield new
+
+# Not in use
+def upgrade_EdgeBinding(edge_binding):
+    """Upgrade EdgeBinding from 0.9.2 to 1.0.0."""
+    for kg_id in ensure_list(edge_binding["kg_id"]):
+        new = {
+            "id": kg_id,
+        }
+        for key, value in edge_binding.items():
+            if key in ("qg_id", "kg_id"):
+                continue
+            new[key] = value
+        yield new
+
+
+
 def upgrade_Result(result):
     """Upgrade Result from 0.9.2 to 1.0.0."""
     result = {**result}
@@ -246,18 +257,6 @@ def upgrade_Result(result):
         **new,
         **result,
     }
-    return new
-
-
-def upgrade_Message(message):
-    """Upgrade Message from 0.9.2 to 1.0.0."""
-    new = dict()
-    if "query_graph" in message:
-        new["query_graph"] = message["query_graph"]
-    if "knowledge_graph" in message:
-        new["knowledge_graph"] = upgrade_KnowledgeGraph(message["knowledge_graph"])
-    if "results" in message:
-        new["results"] = upgrade_results(message["results"])
     return new
 
 
