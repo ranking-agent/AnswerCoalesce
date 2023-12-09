a79021f0fa463fc83c5d907b8857582456054eac
Olawumi <wumirose@gmail.com>
2023-12-09 16:26:17 -0500

    added sample notebook

--- documentation/notebook_functions.py
+++ documentation/notebook_functions.py
@@ -182,3 +182,31 @@ def answerGraphToCytoscape(answer,knowledge_graph):
 
     #print(json.dumps(csGraph, indent=4))
     return csGraph
+
+
+def get_enriched_results(res):
+    return list(filter(lambda message: message['enrichments'], res))
+
+
+
+def get_enrichments2results(r):
+    """
+    params: result_message
+    return: 
+    """
+    results = r["message"]["results"]
+    result_to_enrichments = {}
+    enrichments_to_results = {}
+    for result in results:
+        keys = [key for key in result['node_bindings'] if not 'qnode_id' in result['node_bindings'][key][0]]
+        k = keys[0]
+        identifier = result["node_bindings"][k][0]["id"]
+        name = r["message"]["knowledge_graph"]["nodes"][identifier]["name"]
+        name = identifier
+        result_to_enrichments[name] = result["enrichments"]
+    for result, enrichments in result_to_enrichments.items():
+        for enrichment in enrichments:
+            if enrichment not in enrichments_to_results:
+                enrichments_to_results[enrichment] = []
+            enrichments_to_results[enrichment].append(result)
+    return enrichments_to_results
