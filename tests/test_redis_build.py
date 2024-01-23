05ae8e461dd4129a08db77c2d8d86482398b1ff9
cbizon <bizon@renci.org>
2024-01-23 10:25:15 -0500

    fixed file paths

--- tests/test_redis_build.py
+++ tests/test_redis_build.py
@@ -1,12 +1,22 @@
 import pytest
 from src.graph_coalescence.build_redis_files import go
+import os
 
 def test_redis_build():
-    go(input_edge_file="RedisParseTestData/edges.jsonl",
-       input_node_file="RedisParseTestData/nodes.jsonl",
-       output_prov="RedisParseTestData/prov.txt",
-       output_links="RedisParseTestData/links.txt",
-       output_backlinks="RedisParseTestData/backlinks.txt",
-       output_nodenames="RedisParseTestData/names.txt",
-       output_nodelabels="RedisParseTestData/node_labels.txt",
-       output_category_count="RedisParseTestData/category_count.txt" )
+    jsondir = "RedisParseTestData"
+    edgefile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'edges.jsonl')
+    nodefile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'nodes.jsonl')
+    provfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'prov.txt')
+    linkfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'links.txt')
+    backfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'backlinks.txt')
+    namefile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'names.txt')
+    labelfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'nodelabels.txt')
+    categoryfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), jsondir, 'category_count.txt')
+    go(input_edge_file=edgefile,
+       input_node_file=nodefile,
+       output_prov=provfile,
+       output_links=linkfile,
+       output_backlinks=backfile,
+       output_nodenames=namefile,
+       output_nodelabels=labelfile,
+       output_category_count=categoryfile)
